edc7aebc681123abab65ce43ec8014e1fec6bd0f
Hilary <hilarykodji@gmail.com>
2022-06-21 15:21:19 +0000

    Add database connection to subscribe endpoint test

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use std::net::TcpListener;
-use zero2prod::startup;
+use zero2prod::{configuration::get_configuration, startup};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener: TcpListener =
-        TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    // Panic if we can't read configuration
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    // We have remove the hard-coded `8000` - it's now coming from our settings
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+
+    let listener: TcpListener = TcpListener::bind(address).expect("Failed to bind random port");
     startup::run(listener)?.await
 }
